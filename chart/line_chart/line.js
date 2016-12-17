9da6e17f3307dc9e441c8f23dbbfefd241758ef5
Rich1 <schongking@outlook.com>
2016-12-18 01:07:27 +0800

    曲线图和圆

--- chart/line_chart/line.js
+++ chart/line_chart/line.js
@@ -782,24 +782,25 @@ function drawCurveMap(paint, axis, width, height, params){
 }
 
 function drawCurve(paint, params, width, height, axis){
-    var a = 0.25;
-    var b = 0.25;
+    var speed = 0.1;
+    var a = 0.1;
+    var b = 0.1;
     var pAx, pAy, pBx, pBy;
     var basicX = axis.yLeft;
     var basicXScale = (width-2*axis.yLeft)/axis.xAxis.length;
     var basicY = height - axis.xButton;
     var basicYScale = (height-2*axis.xButton)/axis.yAxis.length;
-    for(var i=0; i<params.length; i++){
+    if(i != (params.length - 1)){
         if(i == 0){
             pAx = params[0].x + (params[1].x - params[0].x)*a;
             pAy = params[0].y + (params[1].y - params[0].y)*a;
             pBx = params[1].x - (params[2].x - params[0].x)*b;
             pBy = params[1].y - (params[2].y - params[0].y)*b;
-        }else if(i == params.length-3){
-            pAx = params[params.length-3].x + (params[params.length - 2].x - params[params.length - 4].x)*a;
-            pAy = params[params.length-3].y + (params[params.length - 2].y - params[params.length - 4].y)*a;
-            pBx = params[params.length-2].x - (params[params.length - 1].x - params[params.length - 3].x)*b;
-            pBy = params[params.length-2].y - (params[params.length - 1].y - params[params.length - 3].y)*b;
+        }else if(i == params.length-2){
+            pAx = params[params.length-2].x + (params[params.length - 1].x - params[params.length - 3].x)*a;
+            pAy = params[params.length-2].y + (params[params.length - 1].y - params[params.length - 3].y)*a;
+            pBx = params[params.length-1].x - (params[params.length - 1].x - params[params.length - 2].x)*b;
+            pBy = params[params.length-1].y - (params[params.length - 1].y - params[params.length - 2].y)*b;
         }else{
             pAx = params[i].x + (params[i+1].x - params[i-1].x)*a;
             pAy = params[i].y + (params[i+1].y - params[i-1].y)*a;
@@ -811,6 +812,274 @@ function drawCurve(paint, params, width, height, axis){
         paint.moveTo(basicX+params[i].x*basicXScale, basicY - (basicYScale*params[i].y));
         paint.bezierCurveTo(basicX+pAx*basicXScale, basicY - (basicYScale*pAy), basicX+pBx*basicXScale, basicY - (pBy*basicYScale), basicX+params[i+1].x*basicXScale, basicY - params[i+1].y*basicYScale);
         paint.stroke();
+    }else{
+        clearInterval(timer2);
+        return;
     }
+    i++;
 }
-drawCurveMap(paint5, axis5, width5, height5, paramsA[0]);
+var i = 0;
+var timer2 = setInterval('drawCurveMap(paint5, axis5, width5, height5, paramsA[0])',300);
+
+//曲线动态图
+
+paramsRun = [
+    {
+        'x':0,
+        'y':Math.random()*10
+    },
+    {
+        'x':1,
+        'y':Math.random()*10
+    },
+    {
+        'x':2,
+        'y':Math.random()*10
+    },
+    {
+        'x':3,
+        'y':Math.random()*10
+    },
+    {
+        'x':4,
+        'y':Math.random()*10
+    },
+    {
+        'x':5,
+        'y':Math.random()*10
+    },
+    {
+        'x':6,
+        'y':Math.random()*10
+    },
+    {
+        'x':7,
+        'y':Math.random()*10
+    },
+    {
+        'x':8,
+        'y':Math.random()*10
+    },
+    {
+        'x':9,
+        'y':Math.random()*10
+    },
+    {
+        'x':10,
+        'y':Math.random()*10
+    },
+    {
+        'x':11,
+        'y':Math.random()*10
+    },
+    {
+        'x':12,
+        'y':Math.random()*10
+    },
+    {
+        'x':13,
+        'y':Math.random()*10
+    },
+    {
+        'x':14,
+        'y':Math.random()*10
+    },
+    {
+        'x':15,
+        'y':Math.random()*10
+    },
+    {
+        'x':16,
+        'y':Math.random()*10
+    },
+    {
+        'x':17,
+        'y':Math.random()*10
+    },
+    {
+        'x':18,
+        'y':Math.random()*10
+    },
+    {
+        'x':19,
+        'y':Math.random()*10
+    }  ,{
+        'x':20,
+        'y':Math.random()*10
+    },
+    {
+        'x':21,
+        'y':Math.random()*10
+    },
+    {
+        'x':22,
+        'y':Math.random()*10
+    },
+    {
+        'x':23,
+        'y':Math.random()*10
+    },
+    {
+        'x':24,
+        'y':Math.random()*10
+    },
+    {
+        'x':25,
+        'y':Math.random()*10
+    },
+    {
+        'x':26,
+        'y':Math.random()*10
+    },
+    {
+        'x':27,
+        'y':Math.random()*10
+    },
+    {
+        'x':28,
+        'y':Math.random()*10
+    },
+    {
+        'x':29,
+        'y':Math.random()*10
+    }    
+];
+
+var xAxisList6 = new Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30);
+var yAxisList6 = new Array(1,2,3,4,5,6,7,8,9,10);
+var axis6 = new Axis();
+axis6.xAxis = xAxisList6;
+axis6.yAxis = yAxisList6;
+axis6.xButton = 10;
+axis6.yLeft = 10;
+
+var div_line6 = document.getElementById('div_line6');
+var line6 = document.getElementById('line6');
+line6.width = div_line6.clientWidth;
+line6.height = div_line6.clientHeight;
+var paint6 = line6.getContext('2d');
+var width6 = line6.clientWidth;
+var height6 = line6.clientHeight;
+
+console.log('width:'+div_line6.clientWidth);
+console.log('height:'+div_line6.clientHeight);
+window.onload = drawAxis(paint6, axis6, width6, height6);
+
+function drawRunCurveMap(paint, axis, width, height, params){
+    drawRunCurve(paint, params, width, height, axis);
+}
+
+function drawRunCurve(paint, params, width, height, axis){
+    var speed = 0.1;
+    var a = 0.1;
+    var b = 0.1;
+    var pAx, pAy, pBx, pBy;
+    var basicX = axis.yLeft;
+    var basicXScale = (width-2*axis.yLeft)/axis.xAxis.length;
+    var basicY = height - axis.xButton;
+    var basicYScale = (height-2*axis.xButton)/axis.yAxis.length;
+    if(j != (params.length-1)){
+        if(j == 0){
+            pAx = params[0].x + (params[1].x - params[0].x)*a;
+            pAy = params[0].y + (params[1].y - params[0].y)*a;
+            pBx = params[1].x - (params[2].x - params[0].x)*b;
+            pBy = params[1].y - (params[2].y - params[0].y)*b;
+        }else if(j == params.length-2){
+            pAx = params[params.length-2].x + (params[params.length - 1].x - params[params.length - 3].x)*a;
+            pAy = params[params.length-2].y + (params[params.length - 1].y - params[params.length - 3].y)*a;
+            pBx = params[params.length-1].x - (params[params.length - 1].x - params[params.length - 2].x)*b;
+            pBy = params[params.length-1].y - (params[params.length - 1].y - params[params.length - 2].y)*b;
+        }else{
+            pAx = params[j].x + (params[j+1].x - params[j-1].x)*a;
+            pAy = params[j].y + (params[j+1].y - params[j-1].y)*a;
+            pBx = params[j+1].x - (params[j+2].x - params[j].x)*b;
+            pBy = params[j+1].y - (params[j+2].y - params[j].y)*b;
+        }
+        paint.strokeStyle = 'yellow';
+        paint.beginPath();
+        paint.moveTo(basicX+params[j].x*basicXScale, basicY - (basicYScale*params[j].y));
+        paint.bezierCurveTo(basicX+pAx*basicXScale, basicY - (basicYScale*pAy), basicX+pBx*basicXScale, basicY - (pBy*basicYScale), basicX+params[j+1].x*basicXScale, basicY - params[j+1].y*basicYScale);
+        paint.stroke();
+    }else{
+        paint.clearRect(0,0,width, height);
+        drawAxis(paint, axis, width, height);
+        for(var v=0; v<params.length; v++){
+            if(v==params.length-1){
+                params[v].y = Math.random()*10; 
+            }else{
+                params[v].y = params[v+1].y;
+            }
+        }
+
+        for(var k=0; k<params.length-1; k++){
+            if(k == 0){
+                pAx = params[0].x + (params[1].x - params[0].x)*a;
+                pAy = params[0].y + (params[1].y - params[0].y)*a;
+                pBx = params[1].x - (params[2].x - params[0].x)*b;
+                pBy = params[1].y - (params[2].y - params[0].y)*b;
+            }else if(k == params.length-2){
+                pAx = params[params.length-2].x + (params[params.length - 1].x - params[params.length - 3].x)*a;
+                pAy = params[params.length-2].y + (params[params.length - 1].y - params[params.length - 3].y)*a;
+                pBx = params[params.length-1].x - (params[params.length - 1].x - params[params.length - 2].x)*b;
+                pBy = params[params.length-1].y - (params[params.length - 1].y - params[params.length - 2].y)*b;
+            }else{
+                pAx = params[k].x + (params[k+1].x - params[k-1].x)*a;
+                pAy = params[k].y + (params[k+1].y - params[k-1].y)*a;
+                pBx = params[k+1].x - (params[k+2].x - params[k].x)*b;
+                pBy = params[k+1].y - (params[k+2].y - params[k].y)*b;
+            }
+            paint.strokeStyle = 'yellow';
+            paint.beginPath();
+            paint.moveTo(basicX+params[k].x*basicXScale, basicY - (basicYScale*params[k].y));
+            paint.bezierCurveTo(basicX+pAx*basicXScale, basicY - (basicYScale*pAy), basicX+pBx*basicXScale, basicY - (pBy*basicYScale), basicX+params[k+1].x*basicXScale, basicY - params[k+1].y*basicYScale);
+            paint.stroke();
+        }
+        //clearInterval(timer2);
+        return;
+    }
+    j++;
+}
+var j = 0;
+var timer3 = setInterval('drawRunCurveMap(paint6, axis6, width6, height6, paramsRun)',300);
+
+//画扇形
+var div_line7 = document.getElementById('div_line7');
+var line7 = document.getElementById('line7');
+line7.width = div_line7.clientWidth;
+line7.height = div_line7.clientHeight;
+var paint7 = line7.getContext('2d');
+var width7 = line7.clientWidth;
+var height7 = line7.clientHeight;
+console.log('width7:'+width7);
+console.log('height7:'+height7);
+
+function drawArc(paint, centerX, centerY, radius, beginDeg, endDeg, direction, color){
+    paint.fillStyle = color;
+    paint.strokeStyle = color;
+    paint.lineWidth = 0;
+    
+    paint.beginPath();
+    paint.moveTo(centerX, centerY);
+    paint.lineTo(centerX+radius*Math.cos(beginDeg), centerY+radius*Math.sin(beginDeg));
+    paint.lineTo(centerX+radius*Math.cos(endDeg), centerY+radius*Math.sin(endDeg));
+    paint.lineTo(centerX, centerY);
+    paint.stroke();
+    paint.fill();
+
+    paint.beginPath();
+    paint.arc(centerX, centerY, radius, beginDeg, endDeg, direction);
+    paint.closePath();
+    paint.fill();
+}
+function drawCicle(paint){
+    drawArc(paint, 100, 100, 80, 0, Math.PI*0.5, false, 'yellow');
+    drawArc(paint, 100, 100, 80, Math.PI*0.5, Math.PI*1, false, 'red');
+    drawArc(paint, 100, 100, 80, Math.PI*1, Math.PI*1.5, false, 'blue');
+    drawArc(paint, 100, 100, 80, Math.PI*1.5, Math.PI*2, false, 'green');
+}
+drawCicle(paint7);
+/*paint7.beginPath();
+paint7.arc(200,200,80,0,Math.PI*0.1,false);
+paint7.fillStyle="white";
+paint7.closePath();
+paint7.fill();*/
